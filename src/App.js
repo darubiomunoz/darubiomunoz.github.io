aaed2108703f0c7ec6b25b4e215cf8966845c246
Diego Rubio <darubiomunoz@gmail.com>
2021-03-10 07:20:59 -0300

    UNDER CONSTRUCTION

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PORTFOLIO UNDER CONSTRUCTION
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/darubiomunoz"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Github
         </a>
       </header>
     </div>
