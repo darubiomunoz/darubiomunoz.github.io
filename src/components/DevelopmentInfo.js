3c5ed73e8d3a4f26744e540b8581167f9907d717
Diego Rubio <darubiomunoz@gmail.com>
2021-03-11 11:51:54 -0300

    spelling correction

--- src/components/DevelopmentInfo.js
+++ src/components/DevelopmentInfo.js
@@ -6,7 +6,7 @@ const DevelopmentInfo = () => {
         <section className="development__body">
             <h1 className="development__title">Coming Soon</h1>
             <h4 className="development__subtitle">The website is under development</h4>
-            <a className="development__calltoaction" href="https://github.com/darubiomunoz" target="_blank" rel="noreferrer">Visit GitHub Account</a>
+            <a className="development__calltoaction" href="https://github.com/darubiomunoz" target="_blank" rel="noreferrer">Visit my github</a>
         </section>
     );
 }
