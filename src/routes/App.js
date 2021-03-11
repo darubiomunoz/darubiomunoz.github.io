fd730d3ebaaf4d85cd8529e27ccf245caa19ff77
Diego Rubio <darubiomunoz@gmail.com>
2021-03-11 06:14:54 -0300

    Router setup

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import HomePage from '../pages/HomePage';
+import AboutMe from '../pages/AboutMe';
+import Portfolio from '../pages/Portfolio';
+import Resume from '../pages/Resume';
+import Contact from '../pages/Contact';
+import UnderDevelopment from '../pages/UnderDevelopment';
+import Page404 from '../pages/Page404';
 
 const App = () => {
   return (
-    <h1>App</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/aboutme" component={AboutMe} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/underdevelopment" component={UnderDevelopment} />
+        <Route component={Page404} />
+      </Switch>
+    </Router>
   );
 }
 
