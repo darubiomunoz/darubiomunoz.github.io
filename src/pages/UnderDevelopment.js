c958424a6af3c13b30be61516581d01ef81fba5a
Diego Rubio <darubiomunoz@gmail.com>
2021-03-11 10:01:10 -0300

    global styles

--- src/pages/UnderDevelopment.js
+++ src/pages/UnderDevelopment.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import './styles/UnderDevelopment.css';
 
 import underDevelopment from '../assets/icons/under_development.svg';
 
 const UnderDevelopment = () => {
     return (
-        <main>
-            <figure>
-                <img className="" src={underDevelopment} loading="lazy" alt="Icon the programmer developing a this website, right now!" />
+        <main className="development">
+            <figure className="img__container--development">
+                <img className="img" src={underDevelopment} loading="lazy" alt="Icon the programmer developing a this website, right now!" />
             </figure>
-            <section>
-                <h1>Coming Soon</h1>
-                <h3>The website is under development</h3>
-                <p>In the meantime visit my <a href="https://github.com/darubiomunoz" target="_blank" rel="noreferrer">GitHub Account</a></p>
+            <section className="development__body">
+                <h1 className="development__title">Coming Soon</h1>
+                <h3 className="development__subtitle">The website is under development</h3>
+                <p className="development__calltoaction">In the meantime visit my <a href="https://github.com/darubiomunoz" target="_blank" rel="noreferrer">GitHub Account</a></p>
             </section>
         </main>
     );
