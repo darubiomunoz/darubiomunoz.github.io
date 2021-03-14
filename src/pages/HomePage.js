53ddc45d68184f01614739c99025d4a45a7e8e86
Diego Rubio <darubiomunoz@gmail.com>
2021-03-14 10:43:37 -0300

    hommepage's background

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './styles/HomePage.css';
+
+import backgroundSmall from '../assets/images/homepage-background-small.jpg';
+import backgroundMedium from '../assets/images/homepage-background-medium.jpg';
+import backgroundLarge from '../assets/images/homepage-background-large.jpg';
 
 const HomePage = () => {
     return(
         <div className="homepage">
-            <picture>
-                <srcset src=""></srcset>
-                <srcset src=""></srcset>
-                <srcset src=""></srcset>
-                <img src={} alt="" />
+            <picture className="homepage__container">
+                <source srcSet={backgroundLarge} media="(min-width: 0px)"></source>
+                <source srcSet={backgroundMedium} media="(min-width: 500px)"></source>
+                <source srcSet={backgroundSmall} media="(min-width: 1024px)"></source>
+                <img className="homepage__background" src={backgroundSmall} alt="background images" />
             </picture>
             <h1>Homepage</h1>
         </div>
