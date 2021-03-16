763d1906ebabce20e994de9f22c8f821db6742bf
Diego Rubio <darubiomunoz@gmail.com>
2021-03-16 08:17:32 -0300

    correction to responsive styles of the header's navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
     const { handleClick, clicked, reference, screen } = ComponentLogic();
 
     return (
-        <nav className="navbar navbar-expand-md position-sticky top-0 start-0 end-100">
+        <nav className="navbar navbar-expand-lg position-sticky top-0 start-0 end-100">
             <div className="container-fluid d-flex justify-content-between align-items-center">
                 <Link className="navbar-brand" to="/">
                     <img className="navbar__logo" src={clicked ? LogoClicked : Logo} alt="Diego Rubio's Logo" />
@@ -44,8 +44,8 @@ const Header = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => handleClick()}>
                     <img className="navbar__menuicon" src={HamburgerIcon} alt="Press to show the website pages" />
                 </button>
-                <div id="navbarNav" className={`navbar__menutab collapse navbar-collapse ${screen >= 1024 ? '' : 'position-absolute d-flex flex-column'} ${screen >= 1024 ? '' : clicked ? '' : 'invisible'}`} ref={reference}>
-                    <img className={`navbar__closeicon ${screen >= 1024 ? 'invisible' : ''}`} src={CloseIcon} alt="Press to hide the website pages" onClick={() => handleClick()} />
+                <div id="navbarNav" className={`navbar__menutab collapse navbar-collapse ${screen >= 992 ? '' : 'position-absolute d-flex flex-column'} ${screen >= 992 ? '' : clicked ? '' : 'invisible'}`} ref={reference}>
+                    <img className={`navbar__closeicon ${screen >= 992 ? 'invisible' : ''}`} src={CloseIcon} alt="Press to hide the website pages" onClick={() => handleClick()} />
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
