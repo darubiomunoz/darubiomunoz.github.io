014b3e6c97de348570e34edb8def972f031f77c6
Diego Rubio <darubiomunoz@gmail.com>
2021-03-16 01:47:59 -0300

    adding logo to the header's navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './styles/Header.css';
 
+import Logo from '../assets/images/logo.svg';
+
 const ComponentLogic = () => {
     const [ clicked, setClicked ] = useState(false);
 
@@ -18,7 +20,9 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-transparent position-sticky top-0 start-0 end-100">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Portfolio</a>
+                <a className="navbar-brand" href="#">
+                    <img src={Logo} alt="" width="30" height="24" />
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => handleClick()}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
