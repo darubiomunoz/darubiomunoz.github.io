ba359787fd55c34123422d525684ccd509dab9bc
Diego Rubio <darubiomunoz@gmail.com>
2021-03-16 05:51:09 -0300

    changing color of logo on interaction

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './styles/Header.css';
 
 import Logo from '../assets/images/logo.svg';
+import LogoClicked from '../assets/images/logo-clicked.svg';
 import HamburgerIcon from '../assets/icons/hamburger-icon.svg';
 import CloseIcon from '../assets/icons/close-icon.svg';
 
@@ -24,7 +25,7 @@ const Header = () => {
         <nav className="navbar navbar-expand-lg position-sticky top-0 start-0 end-100">
             <div className="container-fluid d-flex justify-content-between align-items-center">
                 <Link className="navbar-brand" to="/">
-                    <img className="navbar__logo" src={Logo} alt="Diego Rubio's Logo" />
+                    <img className="navbar__logo" src={clicked ? LogoClicked : Logo} alt="Diego Rubio's Logo" />
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => handleClick()}>
                     <img className="navbar__menuicon" src={HamburgerIcon} alt="Press to show the website pages" />
