a3463df414b951fcbd1fe2b6b95bbad8fd9f45a2
Diego Rubio <darubiomunoz@gmail.com>
2021-03-16 05:39:31 -0300

    Adding close icon and more styles for the header and navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import './styles/Header.css';
 
 import Logo from '../assets/images/logo.svg';
 import HamburgerIcon from '../assets/icons/hamburger-icon.svg';
+import CloseIcon from '../assets/icons/close-icon.svg';
 
 const ComponentLogic = () => {
     const [ clicked, setClicked ] = useState(false);
@@ -20,7 +21,7 @@ const Header = () => {
     const { handleClick, clicked } = ComponentLogic();
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-transparent position-sticky top-0 start-0 end-100">
+        <nav className="navbar navbar-expand-lg position-sticky top-0 start-0 end-100">
             <div className="container-fluid d-flex justify-content-between align-items-center">
                 <Link className="navbar-brand" to="/">
                     <img className="navbar__logo" src={Logo} alt="Diego Rubio's Logo" />
@@ -28,7 +29,8 @@ const Header = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => handleClick()}>
                     <img className="navbar__menuicon" src={HamburgerIcon} alt="Press to show the website pages" />
                 </button>
-                <div id="navbarNav" className={`collapse ${clicked ? 'show' : 'hide'} navbar-collapse`}>
+                <div id="navbarNav" className={`navbar__menutab collapse ${clicked ? '' : 'invisible'} navbar-collapse position-absolute d-flex flex-column`}>
+                    <img className="navbar__closeicon" src={CloseIcon} alt="Press to hide the website pages" onClick={() => handleClick()} />
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
