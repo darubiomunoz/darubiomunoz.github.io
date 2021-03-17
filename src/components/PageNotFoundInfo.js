34bcd2cc4d1f713a84c7dfc42341c0b5244e4b59
Diego Rubio <darubiomunoz@gmail.com>
2021-03-17 01:12:28 -0300

    changing target attribute

--- src/components/PageNotFoundInfo.js
+++ src/components/PageNotFoundInfo.js
@@ -7,7 +7,7 @@ const PageNotFoundInfo = () => {
         <section className="pagenotfound__body">
             <h1 className="pagenotfound__title" tabIndex="0">Page Not Found!</h1>
             <h4 className="pagenotfound__subtitle" tabIndex="0">Error 404.</h4>
-            <Link className="pagenotfound__calltoaction" to="/" target="_blank" rel="noreferrer">Go back to the Homepage!</Link>
+            <Link className="pagenotfound__calltoaction" to="/" rel="noreferrer">Go back to the Homepage!</Link>
         </section>
     );
 }
