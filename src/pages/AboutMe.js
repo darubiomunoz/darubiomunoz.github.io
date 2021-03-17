f20f86126c7ddc5da87b17db063783e00d6e3ad4
Diego Rubio <darubiomunoz@gmail.com>
2021-03-17 03:30:01 -0300

    about me page setup

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import './styles/AboutMe.css';
 
-const AboutMe = () => {}
+import backgroundSmall from '../assets/images/homepage-background-small.jpg';
+import backgroundMedium from '../assets/images/homepage-background-medium.jpg';
+import backgroundLarge from '../assets/images/homepage-background-large.jpg';
+import Header from '../components/Header';
+
+const AboutMe = () => {
+    return(
+        <div className="homepage">
+            <picture className="homepage__container">
+                <source srcSet={backgroundLarge} media="(min-width: 0px)"></source>
+                <source srcSet={backgroundMedium} media="(min-width: 500px)"></source>
+                <source srcSet={backgroundSmall} media="(min-width: 1024px)"></source>
+                <img className="homepage__background" src={backgroundSmall} loading="lazy" alt="background images" />
+            </picture>
+            <Header />
+            <div className="homepage__hero">
+                <p className="homepage__title" tabIndex="0">Welcome,</p>
+                <p className="homepage__title color-option4" tabIndex="0">I'm Diego Rubio</p>
+                <p className="homepage__title margin-bottom" tabIndex="0">A Front-end Developer.</p>
+            </div>
+        </div>
+    );
+};
 
 export default AboutMe;
