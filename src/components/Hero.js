4590aa693eb9b9a62f10b9dec9549b3760d75830
Diego Rubio <darubiomunoz@gmail.com>
2021-03-18 10:40:26 -0300

    Adding accessibility attributes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,8 +8,8 @@ import GmailIcon from '../assets/icons/icon-gmail.svg';
 const Hero = () => {
     return(
         <div className="hero">
-            <h1 className="hero__title">I am Diego Rubio.</h1>
-            <h3 className="hero__text">I am a Front-end Developer specialized in JavaScript. I like learning languages, doing calistecnics/running, reading books about various topics and also I really enjoy programming web applications because it allows me to be very creative in finding solutions during the development process.</h3>
+            <h1 className="hero__title" tabIndex="0">I am Diego Rubio.</h1>
+            <h3 className="hero__text" tabIndex="0">I am a Front-end Developer specialized in JavaScript. I like learning languages, doing calistecnics/running, reading books about various topics and also I really enjoy programming web applications because it allows me to be very creative in finding solutions during the development process.</h3>
             <ul className="hero__list--social">
                 <li className="hero__list--item">
                     <a href="https://www.linkedin.com/in/darmdev/" target="_blank" rel="noreferrer">
