ac1a224ed714bb19139144a0b8e96e805fb21268
Diego Rubio <darubiomunoz@gmail.com>
2021-03-19 08:01:07 -0300

    Fetch data from json bin

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import './styles/CarouselItem.css';
 
-import playIcon from '../assets/icons/icon-github.svg';
+import GithubIcon from '../assets/icons/icon-github.svg';
 import plusIcon from '../assets/icons/icon-linkedin.svg';
 
+const ComponentLogic = () => {
+    const URL = 'https://api.jsonbin.io/b/60548436683e7e079c546027';
+
+    const fetchData = async (_URL) => {
+        const response = await fetch(_URL);
+        const data = await response.json();
+        return data;
+    }
+
+    const data = fetchData(URL);
+
+    return { data }
+}
+
 const CarouselItem = ({ id, cover, title, description, link, code }) => {
+    const { data } = ComponentLogic();
+
     return (
         <div className="carousel-item">
             <img className="carousel-item__img" src={cover} alt={`Image of ${title}`} />
             <div className="carousel-item__details">
                 <div>
-                    <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" /> 
+                    <img className="carousel-item__details--img" src={GithubIcon} alt="Play Icon" /> 
                     <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon" /> 
                 </div>
                 <p className="carousel-item__details--title">{title}</p>
