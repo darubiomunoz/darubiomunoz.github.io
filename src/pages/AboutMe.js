295c0908a070fdf23e78b46fc63fedd32ab7091a
Diego Rubio <darubiomunoz@gmail.com>
2021-03-19 05:36:20 -0300

    Adding styles to the portfolio page and making the basic layout

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -3,21 +3,11 @@ import './styles/AboutMe.css';
 
 import PersonalData from '../components/PersonalData';
 
-const ComponentLogic = () => {
-    const screen = window.screen.width;
-
-    return { screen };
-}
-
-const AboutMe = () => {
-    const { screen } = ComponentLogic();
-
-    return(
-        <div id="aboutme" className="aboutme">
-            <h3 className="aboutme__title">About Me</h3>
-            <PersonalData />
-        </div>
-    );
-};
+const AboutMe = () => (
+    <div id="aboutme" className="aboutme">
+        <h3 className="aboutme__title">About Me</h3>
+        <PersonalData />
+    </div>
+);
 
 export default AboutMe;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import './styles/Portfolio.css';
 
-const Portfolio = () => {}
+const Portfolio = () => (
+    <div id="portfolio" className="portfolio">
+        <h3 className="portfolio__title">Portfolio</h3>
+        <p className="portfolio__subtitle mt-5">Hey! Check out some of my personal projects.</p>
+    </div>
+);
 
 export default Portfolio;
