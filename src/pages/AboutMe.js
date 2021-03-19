1eae3151eb7022d0cab31e97e3f6c949a035c544
Diego Rubio <darubiomunoz@gmail.com>
2021-03-19 01:02:09 -0300

    About me page's title and basic layout, plus fixing responsive style 240px width devices

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -15,7 +15,8 @@ const AboutMe = () => {
             {/* I really enjoy learning languages and I've been studying a lot of French and English the last couple of years, and I wanted to start learning German or Mandarin Chinese in 2020. However, I like to learn languages by immersion, in other words, by travelling and living in places where people speak those language but the last year I could not travel so my curiosity spiked and I started looking for options to learn new things.
             That's how I came across with programming languages. I realized that they aren't so different to traditional languages so I could dive into studying them using the same principle of immersion by building things.
             So far I'm very comfortable using the next technologies: */}
-            <div className="card">
+            <h3 className="aboutme__title">About Me</h3>
+            <div className="card mt-5">
                 <img src="..." className="card-img-top" alt="..." />
                 <div className="card-body">
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
