22ba8da748d6fece8b250af6258858f2ba79f43f
Diego Rubio <darubiomunoz@gmail.com>
2021-03-20 16:08:33 -0300

    Responsive design styles for mobile and tablet devices

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,7 +7,7 @@ import CarouselItem from '../components/CarouselItem';
 const Portfolio = () => (
     <div id="portfolio" className="portfolio">
         <h3 className="portfolio__title">Portfolio</h3>
-        <p className="portfolio__subtitle mt-3">Hey! Check out some of my personal projects.</p>
+        <p className="portfolio__subtitle mt-3">Hey! Check out some of my personal projects. Click the github icon to go to the project's repository or Click the website icon to go to the project's site</p>
         <Carousel>
             <CarouselItem />
         </Carousel>
