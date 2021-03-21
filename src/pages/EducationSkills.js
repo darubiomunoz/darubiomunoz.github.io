5bbf6af7af8bab65daa46f790299b5ba824c74fd
Diego Rubio <darubiomunoz@gmail.com>
2021-03-20 22:29:51 -0300

    Base styles and layout

--- src/pages/EducationSkills.js
+++ src/pages/EducationSkills.js
@@ -1,6 +1,27 @@
-import React from 'react';
-import './styles/EducationSkills.css';
+import React from "react";
+import "./styles/EducationSkills.css";
 
-const EducationSkills = () => {}
+const EducationSkills = () => {
+  return (
+    <div id="educationskills" className="educationskills">
+      <h3 className="educationskills__title" tabIndex="0">
+        Education
+      </h3>
+      <p className="educationskills__subtitle mt-3" tabIndex="0">
+        Hey! Check out some of my personal projects. Click the github icon to go
+        to the project's repository or Click the website icon to go to the
+        project's site
+      </p>
+      <h3 className="educationskills__title" tabIndex="0">
+        Skills
+      </h3>
+      <p className="educationskills__subtitle mt-3" tabIndex="0">
+        Hey! Check out some of my personal projects. Click the github icon to go
+        to the project's repository or Click the website icon to go to the
+        project's site
+      </p>
+    </div>
+  );
+};
 
 export default EducationSkills;
