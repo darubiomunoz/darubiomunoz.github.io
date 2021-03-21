75085d3afd903d618d34ace23d8c782ebfd4a0fb
Diego Rubio <darubiomunoz@gmail.com>
2021-03-21 00:05:23 -0300

    skills mobile

--- src/pages/EducationSkills.js
+++ src/pages/EducationSkills.js
@@ -13,12 +13,12 @@ const EducationSkills = () => {
           development.
         </p>
         <ul className="educationskills__unorderedlist">
-          <li className="educationskills__listitem">
+          <li className="educationskills__listitem--education">
             <h4 className="educationskills__institution">Platzi.com</h4>
             <p className="educationskills__career">Web Development</p>
             <p className="educationskills__date">2020 - 2021</p>
           </li>
-          <li className="educationskills__listitem">
+          <li className="educationskills__listitem--education">
             <h4 className="educationskills__institution">
               Federico Santa María Technical University
             </h4>
@@ -37,6 +37,25 @@ const EducationSkills = () => {
           I'd like to keep adding skills to this list in the coming months.
           Skills like React Native and Node.JS.
         </p>
+        <li className="educationskills__listitem--skill">
+          <p className="educationskills__skill">HTML</p>
+          <p className="educationskills__skill">CSS</p>
+          <p className="educationskills__skill">Bootstrap</p>
+          <p className="educationskills__skill">SASS</p>
+          <p className="educationskills__skill">JavaScript</p>
+          <p className="educationskills__skill">React</p>
+          <p className="educationskills__skill">React Router</p>
+          <p className="educationskills__skill">JSX</p>
+          <p className="educationskills__skill">Markdown</p>
+          <p className="educationskills__skill">Mobile First</p>
+          <p className="educationskills__skill">Responsive Design</p>
+          <p className="educationskills__skill">Web Accessibility</p>
+          <p className="educationskills__skill">NPM</p>
+          <p className="educationskills__skill">SPA</p>
+          <p className="educationskills__skill">English</p>
+          <p className="educationskills__skill">French</p>
+          <p className="educationskills__skill">Spanish</p>
+        </li>
       </section>
     </div>
   );
