3b71b1d8c1acd33b092ad464094da990e5996d72
Diego Rubio <darubiomunoz@gmail.com>
2021-03-20 23:24:46 -0300

    education mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,71 +1,112 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './styles/Header.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./styles/Header.css";
 
-import Logo from '../assets/images/logo.svg';
-import LogoClicked from '../assets/images/logo-clicked.svg';
-import HamburgerIcon from '../assets/icons/hamburger-icon.svg';
-import CloseIcon from '../assets/icons/close-icon.svg';
+import Logo from "../assets/images/logo.svg";
+import LogoClicked from "../assets/images/logo-clicked.svg";
+import HamburgerIcon from "../assets/icons/hamburger-icon.svg";
+import CloseIcon from "../assets/icons/close-icon.svg";
 
 const ComponentLogic = () => {
-    const [ clicked, setClicked ] = useState(false);
-    let reference = useRef(null);
-    const screen = window.screen.width;
+  const [clicked, setClicked] = useState(false);
+  let reference = useRef(null);
+  const screen = window.screen.width;
 
-    const handleClick = () => {
-        if(!clicked) setClicked(true);
-        else setClicked(false);
-    }
+  const handleClick = () => {
+    if (!clicked) setClicked(true);
+    else setClicked(false);
+  };
 
-    useEffect(() => {
-        const handleClickOutside = event => {
-            if(!reference.current.contains(event.target)) setClicked(false);
-        }
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!reference.current.contains(event.target)) setClicked(false);
+    };
 
-        document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
 
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        }
-    }, [])
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
-    return { handleClick, clicked, reference, screen }
-}
+  return { handleClick, clicked, reference, screen };
+};
 
 const Header = () => {
-    const { handleClick, clicked, reference, screen } = ComponentLogic();
+  const { handleClick, clicked, reference, screen } = ComponentLogic();
 
-    return (
-        <nav className="navbar navbar-expand-lg position-sticky top-0 start-0 end-100">
-            <div className="container-fluid d-flex justify-content-between align-items-center">
-                <a className="navbar-brand" href="/">
-                    <img className="navbar__logo" src={clicked ? LogoClicked : Logo} loading="lazy" alt="Diego Rubio website's Logo" />
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => handleClick()}>
-                    <img className="navbar__menuicon" src={HamburgerIcon} loading="lazy" alt="Press to show the website pages" />
-                </button>
-                <div id="navbarNav" className={`navbar__menutab collapse navbar-collapse ${screen >= 992 ? '' : 'position-absolute d-flex flex-column'} ${screen >= 992 ? '' : clicked ? '' : 'invisible'}`} ref={reference}>
-                    <img className={`navbar__closeicon ${screen >= 992 ? 'invisible' : ''}`} src={CloseIcon} loading="lazy" alt="Press to hide the website pages" onClick={() => handleClick()} />
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#aboutme">About Me</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#portfolio">Portfolio</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#education&skills">Education & Skills</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#contact">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav> 
-    );
-} 
+  return (
+    <nav className="navbar navbar-expand-lg position-sticky top-0 start-0 end-100">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <a className="navbar-brand" href="/">
+          <img
+            className="navbar__logo"
+            src={clicked ? LogoClicked : Logo}
+            loading="lazy"
+            alt="Diego Rubio website's Logo"
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => handleClick()}
+        >
+          <img
+            className="navbar__menuicon"
+            src={HamburgerIcon}
+            loading="lazy"
+            alt="Press to show the website pages"
+          />
+        </button>
+        <div
+          id="navbarNav"
+          className={`navbar__menutab collapse navbar-collapse ${
+            screen >= 992 ? "" : "position-absolute d-flex flex-column"
+          } ${screen >= 992 ? "" : clicked ? "" : "invisible"}`}
+          ref={reference}
+        >
+          <img
+            className={`navbar__closeicon ${screen >= 992 ? "invisible" : ""}`}
+            src={CloseIcon}
+            loading="lazy"
+            alt="Press to hide the website pages"
+            onClick={() => handleClick()}
+          />
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#aboutme">
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#educationskills">
+                Education & Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
--- src/pages/EducationSkills.js
+++ src/pages/EducationSkills.js
@@ -4,18 +4,40 @@ import "./styles/EducationSkills.css";
 const EducationSkills = () => {
   return (
     <div id="educationskills" className="educationskills">
-      <h3 className="educationskills__title" tabIndex="0">
-        Education
-      </h3>
-      <p className="educationskills__subtitle mt-3" tabIndex="0">
-        Continuous learning is a constant in my personal and professional development.
-      </p>
-      <h3 className="educationskills__title mt-5" tabIndex="0">
-        Skills
-      </h3>
-      <p className="educationskills__subtitle mt-3" tabIndex="0">
-        I'd like to keep adding skills to this list in the coming months. Skills like React Native and Node.JS.
-      </p>
+      <section className="educationskills__container">
+        <h3 className="educationskills__title" tabIndex="0">
+          Education
+        </h3>
+        <p className="educationskills__subtitle mt-3" tabIndex="0">
+          Continuous learning is a constant in my personal and professional
+          development.
+        </p>
+        <ul className="educationskills__unorderedlist">
+          <li className="educationskills__listitem">
+            <h4 className="educationskills__institution">Platzi.com</h4>
+            <p className="educationskills__career">Web Development</p>
+            <p className="educationskills__date">2020 - 2021</p>
+          </li>
+          <li className="educationskills__listitem">
+            <h4 className="educationskills__institution">
+              Federico Santa María Technical University
+            </h4>
+            <p className="educationskills__career">
+              Construction and management, Bachelor degree
+            </p>
+            <p className="educationskills__date">2012 - 2015</p>
+          </li>
+        </ul>
+      </section>
+      <section className="educationskills__container">
+        <h3 className="educationskills__title mt-5" tabIndex="0">
+          Skills
+        </h3>
+        <p className="educationskills__subtitle mt-3" tabIndex="0">
+          I'd like to keep adding skills to this list in the coming months.
+          Skills like React Native and Node.JS.
+        </p>
+      </section>
     </div>
   );
 };
