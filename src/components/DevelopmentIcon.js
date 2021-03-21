9780d9c6941cafd0964ad94154003f90f7879fec
Diego Rubio <darubiomunoz@gmail.com>
2021-03-21 18:18:36 -0300

    New under development page

--- src/components/DevelopmentIcon.js
+++ src/components/DevelopmentIcon.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './styles/DevelopmentIcon.css';
+import React from "react";
+import "./styles/DevelopmentIcon.css";
 
-import underDevelopment from '../assets/icons/under-development.svg';
+import underDevelopment from "../assets/icons/under-development.svg";
 
-
-const DevelopmentIcon = () => {
-    return(
-        <figure className="img__container--development">
-            <img className="img" src={underDevelopment} loading="lazy" tabIndex="0" alt="Icon the programmer developing a this website, right now!" />
-        </figure>
-    );
-}
+const DevelopmentIcon = () => (
+  <figure className="img__container--development">
+    <img
+      className="img"
+      src={underDevelopment}
+      loading="lazy"
+      tabIndex="0"
+      alt="Icon the programmer developing a this website, right now!"
+    />
+  </figure>
+);
 
 export default DevelopmentIcon;
--- src/pages/UnderDevelopment.js
+++ src/pages/UnderDevelopment.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import './styles/UnderDevelopment.css';
+import React from "react";
+import "./styles/UnderDevelopment.css";
 
-import DevelopmentIcon from '../components/DevelopmentIcon';
-import DevelopmentInfo from '../components/DevelopmentInfo';
+import backgroundLarge from "../assets/images/homepage-background-large.jpg";
+import backgroundMedium from "../assets/images/homepage-background-medium.jpg";
+import backgroundSmall from "../assets/images/homepage-background-small.jpg";
+import DevelopmentIcon from "../components/DevelopmentIcon";
+import DevelopmentInfo from "../components/DevelopmentInfo";
 
 const UnderDevelopment = () => {
-    return (
-        <main className="development">
-            <DevelopmentIcon />
-            <DevelopmentInfo />
-        </main>
-    );
-}
+  return (
+    <main className="development">
+      <picture className="development__container">
+        <source srcSet={backgroundLarge} media="(min-width: 0px)"></source>
+        <source srcSet={backgroundMedium} media="(min-width: 500px)"></source>
+        <source srcSet={backgroundSmall} media="(min-width: 1024px)"></source>
+        <img
+          className="development__background"
+          src={backgroundSmall}
+          loading="lazy"
+          aria-hidden="true"
+          alt="background images"
+        />
+      </picture>
+      <DevelopmentIcon />
+      <DevelopmentInfo />
+    </main>
+  );
+};
 
-export default UnderDevelopment; 
+export default UnderDevelopment;
