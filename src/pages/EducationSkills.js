237f5c9d33645d7717d4c7af41af9d929e20b69b
Diego Rubio <darubiomunoz@gmail.com>
2021-03-20 22:37:53 -0300

    subtitles

--- src/pages/EducationSkills.js
+++ src/pages/EducationSkills.js
@@ -8,17 +8,13 @@ const EducationSkills = () => {
         Education
       </h3>
       <p className="educationskills__subtitle mt-3" tabIndex="0">
-        Hey! Check out some of my personal projects. Click the github icon to go
-        to the project's repository or Click the website icon to go to the
-        project's site
+        Continuous learning is a constant in my personal and professional development.
       </p>
-      <h3 className="educationskills__title" tabIndex="0">
+      <h3 className="educationskills__title mt-5" tabIndex="0">
         Skills
       </h3>
       <p className="educationskills__subtitle mt-3" tabIndex="0">
-        Hey! Check out some of my personal projects. Click the github icon to go
-        to the project's repository or Click the website icon to go to the
-        project's site
+        I'd like to keep adding skills to this list in the coming months. Skills like React Native and Node.JS.
       </p>
     </div>
   );
