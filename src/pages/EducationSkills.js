9e4d8da88f42b4450297283ed2e5a7a1386292ed
Diego Rubio <darubiomunoz@gmail.com>
2021-03-21 00:44:51 -0300

    accessibility features for the education & skills page

--- src/pages/EducationSkills.js
+++ src/pages/EducationSkills.js
@@ -14,18 +14,26 @@ const EducationSkills = () => {
         </p>
         <ul className="educationskills__unorderedlist">
           <li className="educationskills__listitem--education">
-            <h4 className="educationskills__institution">Platzi.com</h4>
-            <p className="educationskills__career">Web Development</p>
-            <p className="educationskills__date">2020 - 2021</p>
+            <h4 className="educationskills__institution" tabIndex="0">
+              Platzi.com
+            </h4>
+            <p className="educationskills__career" tabIndex="0">
+              Web Development
+            </p>
+            <p className="educationskills__date" tabIndex="0">
+              2020 - 2021
+            </p>
           </li>
           <li className="educationskills__listitem--education">
-            <h4 className="educationskills__institution">
+            <h4 className="educationskills__institution" tabIndex="0">
               Federico Santa María Technical University
             </h4>
-            <p className="educationskills__career">
+            <p className="educationskills__career" tabIndex="0">
               Construction and management, Bachelor degree
             </p>
-            <p className="educationskills__date">2012 - 2015</p>
+            <p className="educationskills__date" tabIndex="0">
+              2012 - 2015
+            </p>
           </li>
         </ul>
       </section>
@@ -38,23 +46,57 @@ const EducationSkills = () => {
           Skills like React Native and Node.JS.
         </p>
         <li className="educationskills__listitem--skill">
-          <p className="educationskills__skill">HTML</p>
-          <p className="educationskills__skill">CSS</p>
-          <p className="educationskills__skill">Bootstrap</p>
-          <p className="educationskills__skill">SASS</p>
-          <p className="educationskills__skill">JavaScript</p>
-          <p className="educationskills__skill">React</p>
-          <p className="educationskills__skill">React Router</p>
-          <p className="educationskills__skill">JSX</p>
-          <p className="educationskills__skill">Markdown</p>
-          <p className="educationskills__skill">Mobile First</p>
-          <p className="educationskills__skill">Responsive Design</p>
-          <p className="educationskills__skill">Web Accessibility</p>
-          <p className="educationskills__skill">NPM</p>
-          <p className="educationskills__skill">SPA</p>
-          <p className="educationskills__skill">English</p>
-          <p className="educationskills__skill">French</p>
-          <p className="educationskills__skill">Spanish</p>
+          <p className="educationskills__skill" tabIndex="0">
+            HTML
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            CSS
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            Bootstrap
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            SASS
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            JavaScript
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            React
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            React Router
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            JSX
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            Markdown
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            Mobile First
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            Responsive Design
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            Web Accessibility
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            NPM
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            SPA
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            English
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            French
+          </p>
+          <p className="educationskills__skill" tabIndex="0">
+            Spanish
+          </p>
         </li>
       </section>
     </div>
